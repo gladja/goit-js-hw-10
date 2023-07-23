61aa0fb262184512328d9f0397c8d43e5acd3fa3
gladja <94562072+gladja@users.noreply.github.com>
2023-07-23 16:56:29 +0000

    Deploying to gh-pages from @ gladja/goit-js-hw-10@53a37eb42e1e89a4c3f1a5c0b423d2644428daf1 🚀

--- index.c7cf85da.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/images/search?limit=10").then(o=>o.json()).then(function(o){o.forEach(o=>{console.log(o.url)})}).catch(o=>{console.log("error")});
-//# sourceMappingURL=index.c7cf85da.js.map
